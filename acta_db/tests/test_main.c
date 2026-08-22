6678490c527b12e54da7b1a3ea33380e9d3e7005
Julien BOUCARON <julien.boucaron@gmail.com>
2026-08-23 00:51:16 +0200

    Cleanup

--- acta_db/tests/test_main.c
+++ acta_db/tests/test_main.c
@@ -8,7 +8,7 @@ void run_model_tests(void);
 void run_model_folder_tests(void);
 void run_model_revision_tests(void);
 
-void run_skill_tests(void);
+
 void run_skill_count_tests(void);
 void run_skill_crud_tests(void);
 void run_skill_pagination_tests(void);
@@ -25,27 +25,42 @@ void run_execution_list_tests(void);
 
 int main(void) {
     run_db_tests();
-    run_context_tests();       /* if test_context.c exists */
+
+    // Context
+    run_context_tests();
+
+    // Model folder
     run_model_folder_tests();
+
+    // Model
     run_model_tests();
+
+    // Model Revision
     run_model_revision_tests();
 
+
+    // Skill Folder
     run_skill_folder_tests();
     
-    run_skill_tests(); // Split
+    // Skill
     run_skill_count_tests();
     run_skill_crud_tests();
     run_skill_pagination_tests();
     run_skill_placement_tests();
 
+    // Skill Revision
     run_skill_revision_tests(); 
     
 
+    // Execution
     run_execution_create_tests();
     run_execution_lifecycle_tests();
     run_execution_list_tests();
 
+    // Execution logs
     run_execution_log_tests();
+
+    // Integration
     run_integration_tests();
 
     printf("\n=== ALL TESTS PASSED ===\n");
